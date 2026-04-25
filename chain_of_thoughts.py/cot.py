21fd5f473f39d67a21062a13829a298e47ce8faa
Akshat Singh <nitfactor@gmail.com>
2026-04-25 22:46:19 +0530

    main

--- chain_of_thoughts.py/cot.py
+++ chain_of_thoughts.py/cot.py
@@ -1,18 +1,18 @@
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 
 client = genai.Client()
 
-prompt = """
+system_prompt = """
 You're an expert AI Assistant in resolving user queries using chain of thought.
     You work on START, PLAN and OUPUT steps.
     You need to first PLAN what needs to be done. The PLAN can be multiple steps.
     Once you think enough PLAN has been done, finally you can give an OUTPUT.
 
     Rules:
-    - Strictly Follow the given JSON output format
     - Only run one step at a time.
     - The sequence of steps is START (where user gives an input), PLAN (That can be multiple times) and finally OUTPUT (which is going to the displayed to the user).
 
@@ -22,20 +22,19 @@ You're an expert AI Assistant in resolving user queries using chain of thought.
     Example:
     START: Hey, Can you solve 2 + 3 * 5 / 10
     PLAN: { "step": "PLAN": "content": "Seems like user is interested in math problem" }
-    PLAN: { "step": "PLAN": "content": "looking at the problem, we should solve this using BODMAS method" }
-    PLAN: { "step": "PLAN": "content": "Yes, The BODMAS is correct thing to be done here" }
-    PLAN: { "step": "PLAN": "content": "first we must multiply 3 * 5 which is 15" }
-    PLAN: { "step": "PLAN": "content": "Now the new equation is 2 + 15 / 10" }
-    PLAN: { "step": "PLAN": "content": "We must perform divide that is 15 / 10  = 1.5" }
-    PLAN: { "step": "PLAN": "content": "Now the new equation is 2 + 1.5" }
-    PLAN: { "step": "PLAN": "content": "Now finally lets perform the add 3.5" }
-    PLAN: { "step": "PLAN": "content": "Great, we have solved and finally left with 3.5 as ans" }
     OUTPUT: { "step": "OUTPUT": "content": "3.5" }
-
 """
 
+
+user_input = input("Your prompt: ")
+
+
 response = client.models.generate_content(
-    model="gemini-3-flash-preview", 
-    contents= prompt
+    model="gemini-3-flash-preview",
+    contents=user_input,
+    config=types.GenerateContentConfig(
+        system_instruction=system_prompt
+    )
 )
+
 print(response.text)
