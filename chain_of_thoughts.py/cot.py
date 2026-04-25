a2bbb96833b9c5a8f6069aeb816e7a3f004a7593
Akshat Singh <nitfactor@gmail.com>
2026-04-25 22:30:25 +0530

    main

--- chain_of_thoughts.py/cot.py
+++ chain_of_thoughts.py/cot.py
@@ -1,31 +1,41 @@
-from openai import OpenAI
-import json
+from dotenv import load_dotenv
+from google import genai
 
-client = OpenAI(
-    api_key="AIzaSyBeJc4bXpp2hoAtjywkEgAyhKoK7DNc82Q",
-    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
-)
+load_dotenv()
 
-System_Prompt = """
-You are a senior software engineer.
+client = genai.Client()
+
+prompt = """
+You're an expert AI Assistant in resolving user queries using chain of thought.
+    You work on START, PLAN and OUPUT steps.
+    You need to first PLAN what needs to be done. The PLAN can be multiple steps.
+    Once you think enough PLAN has been done, finally you can give an OUTPUT.
+
+    Rules:
+    - Strictly Follow the given JSON output format
+    - Only run one step at a time.
+    - The sequence of steps is START (where user gives an input), PLAN (That can be multiple times) and finally OUTPUT (which is going to the displayed to the user).
+
+    Output JSON Format:
+    { "step": "START" | "PLAN" | "OUTPUT", "content": "string" }
+
+    Example:
+    START: Hey, Can you solve 2 + 3 * 5 / 10
+    PLAN: { "step": "PLAN": "content": "Seems like user is interested in math problem" }
+    PLAN: { "step": "PLAN": "content": "looking at the problem, we should solve this using BODMAS method" }
+    PLAN: { "step": "PLAN": "content": "Yes, The BODMAS is correct thing to be done here" }
+    PLAN: { "step": "PLAN": "content": "first we must multiply 3 * 5 which is 15" }
+    PLAN: { "step": "PLAN": "content": "Now the new equation is 2 + 15 / 10" }
+    PLAN: { "step": "PLAN": "content": "We must perform divide that is 15 / 10  = 1.5" }
+    PLAN: { "step": "PLAN": "content": "Now the new equation is 2 + 1.5" }
+    PLAN: { "step": "PLAN": "content": "Now finally lets perform the add 3.5" }
+    PLAN: { "step": "PLAN": "content": "Great, we have solved and finally left with 3.5 as ans" }
+    OUTPUT: { "step": "OUTPUT": "content": "3.5" }
 
 """
 
-prompt_text = f"""
-Below is the performance data for Virat Kohli's last 33 matches and other data:
-{json.dumps({}, indent=2)}
-
-Based on this trend, his scoring consistency, and recent run rate, 
-predict how many runs he will score in the next match. 
-Provide a specific number and a short reason for your prediction.
-"""
-
-response = client.chat.completions.create(
-    model="gemini-3-flash-preview",
-    messages=[
-        {"role": "system", "content": System_Prompt},
-        {"role": "user", "content": prompt_text}
-    ]
+response = client.models.generate_content(
+    model="gemini-3-flash-preview", 
+    contents= prompt
 )
-
-print(response.choices[0].message.content)
+print(response.text)
